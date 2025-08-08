cd7582a3b60681640c1973063dea363533fb6471
Cuongvx <vucuonghd1996@gmail.com>
2025-08-08 17:04:23 +0700

    MapViewRepresentable

---
+++ Sources/LifeMapCore/MapViewRepresentable.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewRepresentable.swift
+//  LifeMapCore
+//
+//  Created by sun on 8/8/25.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapViewRepresentable: UIViewRepresentable {
+  
+    typealias UIViewType = MKMapView
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+}
