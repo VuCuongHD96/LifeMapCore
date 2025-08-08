53d21ff5d39ffcebd2d066541cdb0d6b97b9c7e3
Cuongvx <vucuonghd1996@gmail.com>
2025-08-08 16:55:49 +0700

    Init project

--- Sources/LifeMapCore/LifeMapCore.swift
+++ Sources/LifeMapCore/LifeMapCore.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
