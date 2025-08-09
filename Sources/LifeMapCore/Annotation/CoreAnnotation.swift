e28727b0c352579bcad363886bebe490e0b3f8ac
Cuongvx <vucuonghd1996@gmail.com>
2025-08-09 20:24:56 +0700

    map kit drag pin

--- Sources/LifeMapCore/Annotation/CoreAnnotation.swift
+++ Sources/LifeMapCore/Annotation/CoreAnnotation.swift
@@ -9,7 +9,7 @@ import MapKit
 
 class CoreAnnotation: NSObject, MKAnnotation {
     
-    let coordinate: CLLocationCoordinate2D
+    var coordinate: CLLocationCoordinate2D
     
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
