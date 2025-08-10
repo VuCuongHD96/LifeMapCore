a2ad3f7ea54f87edf4e1c4f029ac8e1a2ee252ed
Cuongvx <vucuonghd1996@gmail.com>
2025-08-10 09:11:06 +0700

    drag pin name

--- Sources/LifeMapCore/Annotation/DragPin.swift
+++ Sources/LifeMapCore/Annotation/DragPin.swift
@@ -7,4 +7,18 @@
 
 import MapKit
 
-typealias DragPin = CoreAnnotation
+class DragPin: CoreAnnotation {
+    
+    let id: String
+    var title: String?
+  
+    init(id: String, title: String, coordinate: CLLocationCoordinate2D) {
+        self.id = id
+        self.title = title
+        super.init(coordinate: coordinate)
+    }
+    
+    convenience override init(coordinate: CLLocationCoordinate2D) {
+        self.init(id: "", title: "", coordinate: coordinate)
+    }
+}
--- Sources/LifeMapCore/MapCore/MapCoordinator.swift
+++ Sources/LifeMapCore/MapCore/MapCoordinator.swift
@@ -9,9 +9,27 @@ import MapKit
 
 class MapCoordinator: NSObject, MKMapViewDelegate {
     
+    let locationUseCase = LocationUseCase()
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
         let markerAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "DragPin")
         markerAnnotationView.isDraggable = true
+        markerAnnotationView.canShowCallout = true
         return markerAnnotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        guard let dragPinView = views.first(where: { $0.annotation is DragPin }),
+              let dragPin = dragPinView.annotation as? DragPin else {
+            return
+        }
+
+        if let title = dragPin.title, title.isEmpty {
+            locationUseCase.findMapItem(coordinate: dragPin.coordinate) { updatedPin in
+                mapView.removeAnnotation(dragPin)
+                mapView.addAnnotation(updatedPin)
+                mapView.selectAnnotation(updatedPin, animated: true)
+            }
+        }
     }
 }
